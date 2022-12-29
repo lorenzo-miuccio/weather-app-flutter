b16ade6db4c50379efc811da5467bd7989f097a9
Lorenzo Miuccio <lorenzo.miuccio@sysdata.it>
2022-12-29 09:17:00 +0100

    fix fade transition in weather section

--- weather_app3/lib/pages/weather_home/widgets/all_weather_data.dart
+++ weather_app3/lib/pages/weather_home/widgets/all_weather_data.dart
@@ -7,52 +7,18 @@ import 'package:weather_app/pages/weather_home/widgets/error_widgets/generic_err
 import 'package:weather_app/pages/weather_home/widgets/specific_weather_data_row.dart';
 import 'package:weather_app/providers/weather_bloc.dart';
 
-class AllWeatherData extends StatefulWidget {
+class AllWeatherData extends StatelessWidget {
   const AllWeatherData({Key? key}) : super(key: key);
 
   @override
-  State<AllWeatherData> createState() => _AllWeatherDataState();
-}
-
-class _AllWeatherDataState extends State<AllWeatherData> with TickerProviderStateMixin {
-  late AnimationController _animationController;
-
-  late Animation<double> _animation;
-
-  @override
-  void initState() {
-    super.initState();
-
-    _animationController = AnimationController(
-      duration: const Duration(milliseconds: 350),
-      vsync: this,
-    );
-
-    _animation = Tween<double>(
-      begin: 0.0,
-      end: 1.0,
-    ).animate(
-      CurvedAnimation(parent: _animationController, curve: Curves.easeIn),
-    );
-  }
-
-  @override
-  void dispose() {
-    _animationController.dispose();
-    super.dispose();
-  }
-
-  @override
   Widget build(BuildContext context) {
-    final weatherFetchState = BlocProvider.of<WeatherBloc>(context, listen: true).state;
+    final weatherFetchState = BlocProvider.of<WeatherBloc>(context, listen: false).state;
 
     Weather? currentWeather;
 
     weatherFetchState.maybeWhen((value) => currentWeather = value, orElse: () => currentWeather = null);
 
-    if (currentWeather == null) {
-      return const GenericErrorWidget();
-    }
+    if (currentWeather == null) return const GenericErrorWidget();
 
     final List<SpecificWeatherData> sunDataList = [
       SpecificWeatherData(
@@ -72,36 +38,31 @@ class _AllWeatherDataState extends State<AllWeatherData> with TickerProviderStat
 
     final List<List<SpecificWeatherData>> allSpecificWeatherData = [sunDataList, otherWeatherDataList];
 
-    _animationController.forward();
-
-    return FadeTransition(
-      opacity: _animation,
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          SizedBox(
-            height: 280,
-            child: FadeInImage(
-              fadeInDuration: const Duration(milliseconds: 100),
-              placeholder: const AssetImage('assets/images/placeholder_image.png'),
-              image: NetworkImage(currentWeather!.iconPath),
-              fit: BoxFit.fitHeight,
-            ),
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        SizedBox(
+          height: 280,
+          child: FadeInImage(
+            fadeInDuration: const Duration(milliseconds: 100),
+            placeholder: const AssetImage('assets/images/placeholder_image.png'),
+            image: NetworkImage(currentWeather!.iconPath),
+            fit: BoxFit.fitHeight,
           ),
-          Text(
-            '${currentWeather!.temperature.toStringAsFixed(1)} °C',
-            style: const TextStyle(
-              fontSize: 28,
-              color: Colors.black,
-            ),
+        ),
+        Text(
+          '${currentWeather!.temperature.toStringAsFixed(1)} °C',
+          style: const TextStyle(
+            fontSize: 28,
+            color: Colors.black,
           ),
-          const SizedBox(
-            height: 50,
-          ),
-          ...allSpecificWeatherData.map((dataRow) => SpecificWeatherDataRow(dataRow))
-        ],
-      ),
+        ),
+        const SizedBox(
+          height: 50,
+        ),
+        ...allSpecificWeatherData.map((dataRow) => SpecificWeatherDataRow(dataRow))
+      ],
     );
   }
 }
